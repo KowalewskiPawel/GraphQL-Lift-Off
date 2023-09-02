e6276f9fadb09a386da6c97342ca31049cf26c22
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-09-02 13:20:48 +0300

    add mocks

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,17 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { mocks } from "./MOCKS";
 import { typeDefs } from "./schema";
 
 const startApolloSever = async () => {
-    const apolloServer = new ApolloServer({ typeDefs });
+    const apolloServer = new ApolloServer({
+        schema: addMocksToSchema({
+          schema: makeExecutableSchema({ typeDefs }),
+          mocks
+        }),
+      });
     const { url } = await startStandaloneServer(apolloServer);
 
     console.log(`Apollo Sever running, URL: ${url}`);
