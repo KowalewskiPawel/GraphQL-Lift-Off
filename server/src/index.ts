11701c4e3f4b4e91b608242c7223216d8fd54bdc
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-09-02 13:13:52 +0300

    add ApolloServer

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-//TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+
+const startApolloSever = async () => {
+    const apolloServer = new ApolloServer({ typeDefs });
+    const { url } = await startStandaloneServer(apolloServer);
+
+    console.log(`Apollo Sever running, URL: ${url}`);
+};
+
+startApolloSever();
---
+++ server/src/schema.ts
@@ -0,0 +1,23 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+  # Schema definitions go here
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+  }
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+`;
